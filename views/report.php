c7f21f110e532fb396aa4b4cc530fbc3c44b8ed8
Logan Zehr <zehrl315@outlook.com>
2021-06-25 12:15:47 -0400

    'Call Me' query added to report

--- views/report.php
+++ views/report.php
@@ -10,6 +10,8 @@
 
 <body>
     <h1>Sweetwater Customer Orders Report</h1>
+    <hr>
+
     <h2>Candy Query</h2>
     <p>Shows customer orders that mention "candy" in the comments field.</p>
     <table>
@@ -22,8 +24,8 @@
             
             <!-- Candy Query -->
             <?php
-            $candy_query = "SELECT * FROM sweetwater.sweetwater_test WHERE comments like '%candy%';";
-            $result = $conn->query($candy_query);
+            $query = "SELECT * FROM sweetwater.sweetwater_test WHERE comments like '%candy%';";
+            $result = $conn->query($query);
 
             if ($result->num_rows > 0) {
 
@@ -35,10 +37,43 @@
                 echo "0 results";
             }
 
-            mysqli_close($conn);
+            // mysqli_close($conn);
             ?>
             
         </tr>
+
+    </table>
+
+    <h2>Call Me Query</h2>
+    <p>Shows customer orders that mention "call me."</p>
+    <table>
+        <tr>
+            <th>orderid</th>
+            <th>comments</th>
+        </tr>
+        <tr>
+            <!-- Insert Tables from queries -->
+            
+            <!-- Call Me Query -->
+            <?php
+            $query = "SELECT * FROM sweetwater.sweetwater_test WHERE comments like '%call me%';";
+            $result = $conn->query($query);
+
+            if ($result->num_rows > 0) {
+
+                // output data of each row
+                while ($row = $result->fetch_assoc()) {
+                    echo "<tr><td>" . $row["orderid"] . "</td><td>" . $row["comments"] . "<td><tr>";
+                }
+            } else {
+                echo "0 results";
+            }
+
+            // mysqli_close($conn);
+            ?>
+            
+        </tr>
+
     </table>
 
 </body>
