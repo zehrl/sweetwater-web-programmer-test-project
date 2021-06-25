840e47de994af9b86ea42e40d245c250ceca74d9
Logan Zehr <zehrl315@outlook.com>
2021-06-25 19:39:17 -0400

    DDate parser updates shipdate_expected field

--- utils/date-parser.php
+++ utils/date-parser.php
@@ -5,7 +5,7 @@ $query = "SELECT orderid, comments
     FROM sweetwater.sweetwater_test
     WHERE comments like '%Expected Ship Date: __/__/__%' AND
     shipdate_expected IS NULL";
-    
+
 $result = $conn->query($query);
 
 // Extract date from comments section
@@ -34,11 +34,32 @@ function format_date($date)
     return ($formattedDate);
 }
 
+// Update table with shipdate_expected values
+function update_shipdate_expected($arr)
+{
+
+    global $conn;
+
+    $query = "UPDATE sweetwater.sweetwater_test SET shipdate_expected = '" . $arr["shipdate_expected"] . "' WHERE orderid = '" . $arr["orderid"] . "';";
+
+    // Update shipdate_expected
+    if ($conn -> query($query) === TRUE) {
+        echo "Record updated successfully";
+    } else {
+        echo "Error updating record: " . $conn->error;
+    }
+
+    echo "query: " . $query . "<br>";
+    echo "---------------------------<br>";
+}
+
 if ($result->num_rows > 0) {
 
     while ($row = $result->fetch_assoc()) {
 
         $row["shipdate_expected"] = format_date(extract_date($row));
+
+        update_shipdate_expected($row);
 
         // echo "order: ".$row["orderid"]."<br>comments: ".$row["comments"]."<br>shipdate_expected: ".$row["shipdate_expected"];
         // echo "<br>-------------------------------------------<br>";
