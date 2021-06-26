38bca61d23c8ee532e92c26d2cac7f3f797b0928
Logan Zehr <zehrl315@outlook.com>
2021-06-25 21:41:01 -0400

    Removing date parser to attempt to fix timeout issue on Heroku

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 include "./utils/connection.php";
 
 // Run Date Parser on Database
-include "./utils/date-parser.php";
+// include "./utils/date-parser.php";
 
 // Views
 include "./views/report.php";
